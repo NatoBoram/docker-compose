71041c9a583eb0e4c3ebb4921c8bb3ec9d05409d
Nato Boram <nato.boram+git@gmail.com>
2024-09-13 22:16:22 -0400

    🚚 Move Prometheus file to vmagent

---
+++ helion/cp_fail2ban.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo cp ./fail2ban/filter.d/* /etc/fail2ban/filter.d/
+sudo cp ./fail2ban/jail.d/* /etc/fail2ban/jail.d/
+
+sudo fail2ban-client restart
+
+sudo fail2ban-client status caddy
+sudo fail2ban-client status nextcloud
