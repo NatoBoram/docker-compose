3cf890498ab9d31a5d41151008920f3643498f47
Nato Boram <nato.boram+git@gmail.com>
2025-01-07 02:03:31 -0500

    🔒️ Add Fail2Ban for Jellyfin

--- helion/cp_fail2ban.sh
+++ helion/cp_fail2ban.sh
@@ -5,5 +5,6 @@ sudo cp ./fail2ban/jail.d/* /etc/fail2ban/jail.d/
 sudo fail2ban-client restart
 
 sudo fail2ban-client status caddy
+sudo fail2ban-client status jellyfin
 sudo fail2ban-client status nextcloud
 sudo fail2ban-client status planka
