f42843683cd84428cffacfaebe961ad6804b0b99
Nato Boram <nato.boram+git@gmail.com>
2025-11-05 15:11:34 -0500

    🔨 No down, only up

--- corsair/reload_docker.sh
+++ corsair/reload_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker compose pull
-docker compose down --remove-orphans
-docker compose up -d
+# docker compose down --remove-orphans
+docker compose up --detach --force-recreate --remove-orphans
