baec00d63d4f0339b5de70839f25d88e71bcd214
Nato Boram <nato.boram+git@gmail.com>
2026-06-07 01:36:18 -0400

    ✨ Add forgejo-runner-1
    Signed-off-by: Nato Boram <nato.boram+git@gmail.com>

---
+++ phantom/forgejo/vm.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+
+set -e
+
+mkdir -p /vm/storage
+DISK_IMG="/vm/storage/runner.qcow2"
+SEED_ISO="/vm/storage/seed.iso"
+rm -f "$DISK_IMG" "$SEED_ISO"
+
+# Download cloud image
+mkdir -p /vm/images
+if [ ! -f "/vm/images/$IMAGE_NAME" ]; then
+  echo "Downloading $IMAGE_NAME..."
+  curl --fail --location --output "/vm/images/$IMAGE_NAME" --silent "$IMAGE_URL"
+fi
+
+# Create disk for virtual machine
+qemu-img create -f qcow2 -b "/vm/images/$IMAGE_NAME" -F qcow2 "$DISK_IMG" "$VM_STORAGE"
+
+cat <<EOF >/tmp/user-data
+#cloud-config
+users:
+  - name: runner
+    groups: sudo
+    shell: /bin/bash
+    sudo: ["ALL=(ALL) NOPASSWD:ALL"]
+
+packages:
+  - docker.io
+  - curl
+  - jq
+
+write_files:
+  - path: /vm/storage/config.yaml
+    permissions: "0600"
+    owner: root:root
+    content: |
+      runner:
+        capacity: $VM_CPU
+        labels:
+          - docker:docker://node:lts
+          - ubuntu-latest:docker://node:lts
+
+      container:
+        enable_ipv6: true
+        privileged: true
+
+      server:
+        connections:
+          homelab:
+            url: "$FORGEJO_INSTANCE_URL"
+            uuid: "$FORGEJO_RUNNER_UUID"
+            token: "$FORGEJO_RUNNER_TOKEN"
+
+runcmd:
+  # Forgejo Runner: https://forgejo.org/docs/latest/admin/actions/installation/binary
+  - export ARCH=\$(uname -m | sed 's/x86_64/amd64/;s/aarch64/arm64/')
+  - export RUNNER_VERSION=\$(curl -X 'GET' https://data.forgejo.org/api/v1/repos/forgejo/runner/releases/latest | jq .name -r | cut -c 2-)
+  - export FORGEJO_URL="https://code.forgejo.org/forgejo/runner/releases/download/v\${RUNNER_VERSION}/forgejo-runner-\${RUNNER_VERSION}-linux-\${ARCH}"
+
+  - curl --fail --location --output /usr/local/bin/forgejo-runner --silent \${FORGEJO_URL}
+  - chmod +x /usr/local/bin/forgejo-runner
+
+  - forgejo-runner --config /vm/storage/config.yaml one-job --wait
+  - systemctl poweroff
+EOF
+
+echo "local-hostname: $VM_HOSTNAME" >/tmp/meta-data
+
+cloud-localds "$SEED_ISO" /tmp/user-data /tmp/meta-data
+
+exec qemu-system-x86_64 \
+  -enable-kvm \
+  -cpu host \
+  -smp "$VM_CPU" \
+  -m "$VM_RAM" \
+  -drive file="$DISK_IMG",if=virtio,format=qcow2 \
+  -drive file="$SEED_ISO",format=raw,if=virtio,readonly=on \
+  -device virtio-net-pci,netdev=vmnic \
+  -netdev user,id=vmnic,hostfwd=tcp::22-:22 \
+  -nographic \
+  -snapshot
---
+++ phantom/rand.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Password (bcrypt):"
+printf '%s\n' $(openssl rand 256 | tr -dc '[:graph:]' | tr -d "\'\"\\\#\$" | head -c 72)
+echo
+
+echo "Key (base64):"
+printf '%s\n' $(openssl rand 66 | openssl enc -A -base64)
+echo
+
+echo "Key (hex):"
+openssl rand -hex 64
+echo
